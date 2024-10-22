3c04a582d6b9bc0cad48bdfbafad98842a5f3f51
MihaiAlexandruMilitaru <mmilitaru@griddynamics.com>
2024-10-22 18:49:30 +0300

    refactor sql DAG

--- dags/sql.py
+++ dags/sql.py
@@ -8,7 +8,7 @@ from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-from PostgreSQLCountRows import PostgreSQLCountRows
+from PostgreSQLCountRows import PostgreSQLCountRowsOperator
 from resources import *
 
 
@@ -16,18 +16,13 @@ def get_current_user(ti):
     user = subprocess.check_output('whoami')
     ti.xcom_push(key="user", value=user.decode('utf-8'))
 
-
-def check_table_exist(sql_to_check_table_exist,
-                      table_name):
-    """ callable function to get schema name and after that check if table exist """
-    hook = PostgresHook()
-    # check table exist
-    query = hook.get_first(sql=sql_to_check_table_exist.format(table_name))
-    print(query)
-    # if query:
-    #     return 'dummy_operator'
-
-    return 'create_table'
+def check_table_exists(table_name):
+    hook = PostgresHook(postgres_conn_id="postgres_default")
+    sql = f"SELECT table_name FROM information_schema.tables WHERE table_name = '{table_name}'"
+    query = hook.get_first(sql)
+    if query:
+        return "insert_new_row"
+    return "create_table"
 
 
 def number_of_rows(ti):
@@ -42,55 +37,45 @@ with DAG(
         start_date=config["dag_id_1"]["start_date"],
         schedule=config["dag_id_1"]["schedule_interval"]
 ) as dag:
-    task1 = BashOperator(
-        task_id="print_process_start",
-        bash_command='echo "Process start"'
+    log_start = BashOperator(
+        task_id="log_start",
+        bash_command="echo 'Start processing SQL files'",
+        dag=dag
     )
-    task2 = PythonOperator(
+    get_current_user = PythonOperator(
         task_id="get_current_user",
         python_callable=get_current_user,
-        provide_context=True
-    )
-    task3 = BranchPythonOperator(
-        task_id="check_if_table_exists",
-        python_callable=check_table_exist,
-        op_args=[
-                 """
-                    SELECT COUNT(*)
-                    FROM information_schema.tables
-                    WHERE table_name = '{0}'
-                    """, "table_name"],
+        provide_context=True,
         dag=dag
     )
-    task4 = EmptyOperator(
-        task_id='dummy_operator',
+    check_table_exists = BranchPythonOperator(
+        task_id="check_table_exists",
+        python_callable=check_table_exists,
+        op_args=["table_name"],
         dag=dag
     )
-    task5 = SQLExecuteQueryOperator(
+    create_table = SQLExecuteQueryOperator(
         task_id="create_table",
-        conn_id="postgres_default",
+        conn_id="airflow_postgres_conn",
         sql="""CREATE TABLE table_name(custom_id integer NOT NULL,
-                user_name VARCHAR (50) NOT NULL, timestamp TIMESTAMP NOT NULL);""",
+                        user_name VARCHAR (30) NOT NULL, timestamp TIMESTAMP NOT NULL);""",
         dag=dag
     )
-    task6 = SQLExecuteQueryOperator(
+    insert_new_row = SQLExecuteQueryOperator(
         task_id="insert_new_row",
         conn_id="postgres_default",
-        trigger_rule='none_failed',
-        sql="INSERT INTO table_name VALUES(%s, %s, %s);",
-        parameters=(uuid.uuid4().int % 123456789, uuid.uuid4().hex[:10], datetime.now()),
+        trigger_rule="none_failed",
+        sql="""INSERT INTO table_name(custom_id, user_name, timestamp)
+               VALUES (1, 'user1', CURRENT_TIMESTAMP);""",
         dag=dag
     )
-    task7 = PostgreSQLCountRows(
-        task_id="query_table",
+    count_rows = PostgreSQLCountRowsOperator(
+        task_id="count_rows",
+        conn_id="postgres_default",
         table_name="table_name",
         dag=dag
     )
 
-    task1 >> task2
-    task2 >> task3
-    task3 >> task4
-    task3 >> task5
-    task4 >> task6
-    task5 >> task6
-    task6 >> task7
+    log_start >> get_current_user >> check_table_exists
+    check_table_exists >> create_table >> insert_new_row >> count_rows
+    check_table_exists >> insert_new_row >> count_rows
